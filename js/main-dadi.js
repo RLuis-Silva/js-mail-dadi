c56cf192380980be622294a3eab95aedbdc08570
Luis Silva <s.ros.luis@gmail.com>
2020-10-15 17:16:33 +0200

    add risultato della giocata

--- js/main-dadi.js
+++ js/main-dadi.js
@@ -1,5 +1,6 @@
 console.log("funziona js es. Dadi");
-
+var output;
+var risultatoAI, risultatoGiocatore;
 var numRandomPlayer = Math.floor(Math.random() * 6) + 1; 
 // Math.floor(Math.random() * (max - min) ) + min;
 // valori tra min (incluso) e max (escluso)
@@ -8,10 +9,25 @@ console.log("Giocatore lancia: ", numRandomPlayer);
 var numRandomAI = Math.floor(Math.random() * 6) + 1;
 console.log("Ai lancia: ", numRandomAI);
 
+
+
 if(numRandomAI > numRandomPlayer){
-    console.log("Hai perso!");
+    //console.log("Hai perso!");
+    risultatoAI = "AI lancia: " + numRandomAI;
+    risultatoGiocatore = "Giocatore lancia:" + numRandomPlayer;
+    output = "Hai perso, gli Dei non ti amano!";
 }else if(numRandomAI < numRandomPlayer){
-    console.log("Hai vinto!");
+    //console.log("Hai vinto!");
+    risultatoAI = "AI lancia: " + numRandomAI;
+    risultatoGiocatore = "Giocatore lancia:" + numRandomPlayer;
+    output = "Hai Vinto, sei baciato dalla fortuna!"
 }else{
-    console.log("Pareggio!");
+    //console.log("Pareggio, aaah per Gli Dei!");
+    risultatoAI = "AI lancia: " + numRandomAI;
+    risultatoGiocatore = "Giocatore lancia:" + numRandomPlayer;
+    output = "Pareggio, aaah per Gli Dei!";
 }
+
+document.getElementById('AI').innerHTML = risultatoAI;
+document.getElementById('giocatore').innerHTML = risultatoGiocatore;
+document.getElementById('vittoria').innerHTML = output;
