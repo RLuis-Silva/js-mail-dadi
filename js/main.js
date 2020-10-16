f290432977e59904275ac8f38db4f241d5b6e56e
Luis Silva <s.ros.luis@gmail.com>
2020-10-16 10:21:40 +0200

    edit color : output es.email si vedono
    problema con for alternativo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//console.log("js funziona!");
+console.log("js funziona!");
 /* primo esercizio  */
 
 
@@ -8,15 +8,34 @@ var emailValida = false;
 
 emailUtente = prompt("Inserire la tua email di accesso: ");
 
-for(var i = 0; i <= listaEmailAdmin.length - 1; i++){
-        if(listaEmailAdmin[i] == emailUtente){
-           emailValida = true;
-        }
+// for(var i = 0; i <= listaEmailAdmin.length - 1; i++){
+//         if(listaEmailAdmin[i] == emailUtente){
+//            emailValida = true;
+//         }
+// }
+// if(emailValida == true){
+//     console.log("Benvenuto, " + emailUtente);
+//     document.getElementById('risultato').innerHTML = "Benvenuto, " + emailUtente;
+// }else {
+//     console.log("Errore, hai inserito una email errata!");
+//     document.getElementById('risultato').innerHTML = "Errore, hai inserito una email errata!";
+// }
+
+
+
+// alternativa
+// NOTA: non mi partono gli output console e document
+console.log("hai digitato: " + emailUtente);
+for(var i = 0; i < listaEmailAdmin; i++){
+    console.log("Ciaoooo sono dentro al loop for!");
+    if(listaEmailAdmin[i] == emailUtente){
+        emailUtente = true;
+        console.log("Benvenuto " +  emailUtente);
+        document.getElementById('risultato').innerHTML = "Benvenuto, " + emailUtente;
+    }
 }
-if(emailValida == true){
-    console.log("Benvenuto, " + emailUtente);
-    document.getElementById('risultato').innerHTML = "Benvenuto, " + emailUtente;
-}else {
-    console.log("Errore, hai inserito una email errata!");
-    document.getElementById('risultato').innerHTML = "Errore, hai inserito una email errata!";
+
+if(emailUtente === false){
+   console.log("Errore, hai inserito una email errata!");
+   document.getElementById('risultato').innerHTML = "Errore, hai inserito una email errata!";
 }
