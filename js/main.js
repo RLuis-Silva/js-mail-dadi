c60e27474f1157024295a237e630eb1a6fe231a2
Luis Silva <s.ros.luis@gmail.com>
2020-10-16 10:27:41 +0200

    nulla

--- js/main.js
+++ js/main.js
@@ -8,34 +8,35 @@ var emailValida = false;
 
 emailUtente = prompt("Inserire la tua email di accesso: ");
 
-// for(var i = 0; i <= listaEmailAdmin.length - 1; i++){
-//         if(listaEmailAdmin[i] == emailUtente){
-//            emailValida = true;
-//         }
-// }
-// if(emailValida == true){
-//     console.log("Benvenuto, " + emailUtente);
-//     document.getElementById('risultato').innerHTML = "Benvenuto, " + emailUtente;
-// }else {
-//     console.log("Errore, hai inserito una email errata!");
-//     document.getElementById('risultato').innerHTML = "Errore, hai inserito una email errata!";
-// }
+for(var i = 0; i <= listaEmailAdmin.length - 1; i++){
+        if(listaEmailAdmin[i] == emailUtente){
+           emailValida = true;
+        }
+}
+if(emailValida == true){
+    console.log("Benvenuto, " + emailUtente);
+    document.getElementById('risultato').innerHTML = "Benvenuto, " + emailUtente;
+}else {
+    console.log("Errore, hai inserito una email errata!");
+    document.getElementById('risultato').innerHTML = "Errore, hai inserito una email errata!";
+}
 
 
 
 // alternativa
 // NOTA: non mi partono gli output console e document
-console.log("hai digitato: " + emailUtente);
-for(var i = 0; i < listaEmailAdmin; i++){
-    console.log("Ciaoooo sono dentro al loop for!");
-    if(listaEmailAdmin[i] == emailUtente){
-        emailUtente = true;
-        console.log("Benvenuto " +  emailUtente);
-        document.getElementById('risultato').innerHTML = "Benvenuto, " + emailUtente;
-    }
-}
 
-if(emailUtente === false){
-   console.log("Errore, hai inserito una email errata!");
-   document.getElementById('risultato').innerHTML = "Errore, hai inserito una email errata!";
-}
+// console.log("hai digitato: " + emailUtente); 
+// for(var i = 0; i < listaEmailAdmin; i++){
+//     console.log("Ciaoooo sono dentro al loop for!");
+//     if(listaEmailAdmin[i] == emailUtente){
+//         emailUtente = true;
+//         console.log("Benvenuto " +  emailUtente);
+//         document.getElementById('risultato').innerHTML = "Benvenuto, " + emailUtente;
+//     }
+// }
+
+// if(emailUtente === false){
+//    console.log("Errore, hai inserito una email errata!");
+//    document.getElementById('risultato').innerHTML = "Errore, hai inserito una email errata!";
+// }
